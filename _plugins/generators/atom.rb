73b93fc94b723cc4e39ea807d7126dbd89a75c92
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-04-15 18:44:37 -0400

    Fixed atom.xml generator

--- _plugins/generators/atom.rb
+++ _plugins/generators/atom.rb
@@ -53,8 +53,7 @@ module Jekyll
 
     # Returns the XML header.
     def generate_header
-      '
-<?xml version="1.0" encoding="utf-8"?>
+      '<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
 '
     end
