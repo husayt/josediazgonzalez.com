a7953f8614114cfdb9da856072438c03cf6153fe
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-04-22 20:30:28 -0400

    Reorganized repository to conform to cimino standards

--- _plugins/extensions/liquid_exception_handler.rb
+++
@@ -1,8 +0,0 @@
-# Title: Liquid Exception Handler Jekyll Extension
-# Description: Fixes https://github.com/mojombo/jekyll/issues/46 and https://github.com/mojombo/jekyll/issues/388
-
-module Jekyll
-  class Post
-    attr_accessor :name
-  end
-end
